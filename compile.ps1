751f3768a203e484c53aac4008733e5e95d81510
root <root@WIN-1J6F5NF0R04>
2025-01-08 03:36:21 +0300

    compiled

--- compile.ps1
+++ compile.ps1
@@ -1,32 +1,18 @@
-# Проверяем наличие установленного модуля MS Office
-if (-not (Get-Module -ListAvailable | Where-Object { $_.Name -eq "MSOnline" })) {
-    Write-Host "Не найден модуль MS Online. Установите его командой Install-Module MSOnline"
-}
+# Удаляем все предыдущие PDF-файлы в текущем каталоге и его подкаталогах
+Get-ChildItem -Path $currentDir -Filter *.pdf -Recurse | Remove-Item -Force
+# Ищем все файлы .pptx в текущем каталоге
+$pptFiles = Get-ChildItem -Path $(Get-Location) -Filter *.pptx  -Recurse 
 
 Add-Type -AssemblyName "Microsoft.Office.Interop.PowerPoint"
 $powerpoint = New-Object -ComObject PowerPoint.Application
-
-# Получаем текущий каталог
-$currentDir = Get-Location
-
-# Ищем все файлы .pptx в текущем каталоге
-$pptFiles = Get-ChildItem -Path $currentDir -Filter *.pptx
-
 foreach ($file in $pptFiles) {
     # Открываем файл презентации
     $presentation = $powerpoint.Presentations.Open($file.FullName)
-    
-    # Формируем имя выходного файла PDF
-    $outputFile = Join-Path $currentDir "$($file.BaseName).pdf"
-    
     # Конвертируем презентацию в PDF
+    $outputFile = $file.FullName.Replace(".pptx", ".pdf")
     $presentation.SaveAs($outputFile, [Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType]::ppSaveAsPDF)
-    
     # Закрываем презентацию
     $presentation.Close()
-    
-    Write-Host "Конвертирован файл: $($file.Name)"
 }
-
 # Завершаем работу приложения PowerPoint
 $powerpoint.Quit()
